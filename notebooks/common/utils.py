8531e4d6f32db424a7bb975c7a1d1d7ea3300669
Banibrata De <banibrata@outlook.com>
2018-05-29 15:53:45 -0700

    Add cuda and cudnn version information for windows
    Added CUDA and CUDNN version support for Windows, refactored get_cudnn_version() removing duplicate code

--- notebooks/common/utils.py
+++ notebooks/common/utils.py
@@ -37,70 +37,52 @@ def get_gpu_name():
 def get_cuda_version():
     """Get CUDA version"""
     if sys.platform == 'win32':
-        raise NotImplementedError("Implement this!")
+        cuda=!ls "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA"
+        path = "C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\" + str(cuda[0]) +"\\version.txt"
     elif sys.platform == 'linux' or sys.platform == 'darwin':
         path = '/usr/local/cuda/version.txt'
-        if os.path.isfile(path):
-            with open(path, 'r') as f:
-                data = f.read().replace('\n','')
-            return data
-        else:
-            return "No CUDA in this machine"
     else:
         raise ValueError("Not in Windows, Linux or Mac")
-
+    if os.path.isfile(path):
+        with open(path, 'r') as f:
+            data = f.read().replace('\n','')
+        return data
+    else:
+        return "No CUDA in this machine"
 
 def get_cudnn_version():
     """Get CUDNN version"""
     if sys.platform == 'win32':
-        raise NotImplementedError("Implement this!")
+        cuda=!ls "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA"
+        candidates = ["C:\\Program Files\\NVIDIA GPU Computing Toolkit\\CUDA\\" + str(cuda[0]) +"\\include\\cudnn.h"]
     elif sys.platform == 'linux':
         candidates = ['/usr/include/x86_64-linux-gnu/cudnn_v[0-99].h',
                       '/usr/local/cuda/include/cudnn.h',
                       '/usr/include/cudnn.h']
-        for c in candidates:
-            file = glob.glob(c)
-            if file: break
-        if file:
-            with open(file[0], 'r') as f:
-                version = ''
-                for line in f:
-                    if "#define CUDNN_MAJOR" in line:
-                        version = line.split()[-1]
-                    if "#define CUDNN_MINOR" in line:
-                        version += '.' + line.split()[-1]
-                    if "#define CUDNN_PATCHLEVEL" in line:
-                        version += '.' + line.split()[-1]
-            if version:
-                return version
-            else:
-                return "Cannot find CUDNN version"
-        else:
-            return "No CUDNN in this machine"
     elif sys.platform == 'darwin':
         candidates = ['/usr/local/cuda/include/cudnn.h',
                       '/usr/include/cudnn.h']
-        for c in candidates:
-            file = glob.glob(c)
-            if file: break
-        if file:
-            with open(file[0], 'r') as f:
-                version = ''
-                for line in f:
-                    if "#define CUDNN_MAJOR" in line:
-                        version = line.split()[-1]
-                    if "#define CUDNN_MINOR" in line:
-                        version += '.' + line.split()[-1]
-                    if "#define CUDNN_PATCHLEVEL" in line:
-                        version += '.' + line.split()[-1]
-            if version:
-                return version
-            else:
-                return "Cannot find CUDNN version"
-        else:
-            return "No CUDNN in this machine"
     else:
         raise ValueError("Not in Windows, Linux or Mac")
+    for c in candidates:
+        file = glob.glob(c)
+        if file: break
+    if file:
+        with open(file[0], 'r') as f:
+            version = ''
+            for line in f:
+                if "#define CUDNN_MAJOR" in line:
+                    version = line.split()[-1]
+                if "#define CUDNN_MINOR" in line:
+                    version += '.' + line.split()[-1]
+                if "#define CUDNN_PATCHLEVEL" in line:
+                    version += '.' + line.split()[-1]
+        if version:
+            return version
+        else:
+            return "Cannot find CUDNN version"
+    else:
+        return "No CUDNN in this machine"
 
 
 
